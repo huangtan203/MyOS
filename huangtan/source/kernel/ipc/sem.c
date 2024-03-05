ee3a20bcc54f21e0a49aca3c6f3827ad13c4bc18
huangtan203 <962702339@qq.com>
2024-03-05 22:35:26 +0800

    add sem_wait function

--- huangtan/source/kernel/ipc/sem.c
+++ huangtan/source/kernel/ipc/sem.c
@@ -6,3 +6,33 @@ void sem_init(sem_t *sem, int count){
     sem->count = count;
     list_init(&sem->wait_list);
 }
+void sem_wait(sem_t*sem){
+    irq_state_t state=irq_enter_protection();
+    task_t*task=task_current();
+    if(sem->count<=0){
+        //
+        task_set_block(task);
+        list_insert_last(&sem->wait_list,&task->wait_node);
+        task_dispatch();
+    }
+    sem->count--;
+    irq_leave_protection(state);
+}
+void sem_notify(sem_t*sem){
+    irq_state_t state=irq_enter_protection();
+    if(list_count(&sem->wait_list)){
+        list_node_t*node=list_remove_first(&sem->wait_list);
+        task_t*task=list_node_parent(node,task_t,wait_node);
+        task_set_ready(task);
+        task_dispatch();
+    }else{
+        sem->count++;
+    }
+    irq_leave_protection(state);
+}
+int sem_count(sem_t*sem){
+    irq_state_t irq_state=irq_enter_protection();
+    int count=sem->count;
+    irq_leave_protection(irq_state);
+    return count;
+}
